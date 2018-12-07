bf60443067460ee02efa367b3a7d65b8a7a0b70d
jungjune <willtriti03@naver.com>
2018-12-08 08:10:50 +0900

    Mains

---
+++ Kungfu/Assets/Source/Splash/SceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+    float time = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (time >= 0.5f) {
+            Application.LoadLevel("MainScene");
+        }
+        time += Time.deltaTime;
+    }
+}
