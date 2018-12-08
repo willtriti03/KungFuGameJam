64efe84b3a8f723bd0fb1004427943c1d46158b6
jungjune <willtriti03@naver.com>
2018-12-08 16:44:31 +0900

    올라가랏

--- Kungfu/Assets/Source/Stage/GageManager.cs
+++ Kungfu/Assets/Source/Stage/GageManager.cs
@@ -4,83 +4,56 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GageManager : MonoBehaviour {
-    public Image player1_gage;
-    public Image player2_gage;
+    public Image player_gage;
 
-    public float player1_health = 100f;
-    public float player2_health = 100f;
+    public float player_health = 100f;
 
-    float full_damage1 = 0f,damage1=0;
-    float full_damage2 = 0f, damage2=0;
-    float cal1=100f, cal2=100f;
+
+    float full_damage = 0f,damage=0;
+    float cal = 100f;
     public float speed = 100f;
    
     public bool test = false;
 
 	// Use this for initialization
 	void Start () {
-        player1_gage.GetComponent<Image>().fillAmount = player1_health / 100f;
-        player2_gage.GetComponent<Image>().fillAmount = player2_health / 100f;
+        player_gage.GetComponent<Image>().fillAmount = player_health / 100f;
+     
 
     }
 
     // Update is called once per frame
     void Update () {
         if (test) {
-            DamagePlayer1(30);
-            DamagePlayer2(30);
-
+            DamagePlayer(30);
             test = false;
         }
-        if (player1_health < cal1) {
-            player1_gage.GetComponent<Image>().fillAmount = (cal1) / 100f;
-            player1_health = cal1;
-            full_damage1 = 0;
-            damage1 = 0;
+        if (player_health < cal) {
+            player_gage.GetComponent<Image>().fillAmount = (cal) / 100f;
+            player_health = cal;
+            full_damage = 0;
+            damage = 0;
         }
-        else if (full_damage1 != damage1 && damage1 != 0)
+        else if (full_damage != damage && damage != 0)
         {
-            full_damage1 += Time.deltaTime * speed;
-            player1_health -= Time.deltaTime * speed;
-            player1_gage.GetComponent<Image>().fillAmount = (player1_health) / 100f;
+            full_damage += Time.deltaTime * speed;
+            player_health -= Time.deltaTime * speed;
+            player_gage.GetComponent<Image>().fillAmount = (player_health) / 100f;
 
         }
-        else if (full_damage1 == damage1 && damage1 != 0)
+        else if (full_damage == damage && damage != 0)
         {
-            full_damage1 = 0;
-            damage1 = 0;
+            full_damage = 0;
+            damage = 0;
         }
 
-        //////////////////////////////////////////////////////////////////////////////////////////
-        if (player2_health < cal2)
-        {
-            player2_gage.GetComponent<Image>().fillAmount = (cal2) / 100f;
-            player2_health = cal2;
-            full_damage2 = 0;
-            damage2 = 0;
-        }
-        else if (full_damage2 != damage2 && damage2 != 0)
-        {
-            full_damage2 += Time.deltaTime * speed;
-            player2_health -= Time.deltaTime * speed;
-            player2_gage.GetComponent<Image>().fillAmount = (player2_health) / 100f;
-
-        }
-        else if (full_damage2 == damage2 && damage2 != 0)
-        {
-            full_damage2 = 0;
-            damage2 = 0;
-        }
+      
 
     }
-    public void DamagePlayer1(int damage) {
-        damage1 += damage;
-        cal1 -= damage;
+    public void DamagePlayer(int damage) {
+        this.damage += damage;
+        cal -= damage;
     }
 
-    public void DamagePlayer2(int damage)
-    {
-        damage2 += damage;
-        cal2 -= damage;
-    }
+    
 }
