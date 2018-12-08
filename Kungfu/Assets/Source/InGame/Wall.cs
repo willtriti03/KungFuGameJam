4fbfa30e4654eea8bad430e21f9d3aaf6c19fd8f
jungjune <willtriti03@naver.com>
2018-12-08 18:46:52 +0900

    커밋하고싶다

---
+++ Kungfu/Assets/Source/InGame/Wall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+    public int viewSide;
+    public float speed = 0.1f;
+    public float curTime = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        curTime += Time.deltaTime;
+        if (curTime > 30f)
+        {
+        }
+        else if (curTime>20) {
+            transform.Translate(viewSide * Vector3.right * speed * Time.deltaTime);
+
+        }
+    }
+}
